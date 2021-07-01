cd22aa21e57bc7078d189de9ffc17f49aea76e61
Aurelie Moraitis <amo@keystone-solutions.be>
2021-07-01 10:08:16 +0200

    modify App and index components

---
+++ patisserie/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {GlobalStyle} from "./styles/GlobalStyle";
+// import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      {/* <Header/> */}
+      <header>TEST</header>
+
+    </div>
+  );
+}
+
+export default App;
